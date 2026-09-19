91c7845fa05ffaa68f9b566de77de881b328f155
Nguyen Vu An <nguyenvuan2005@gmail.com>
2026-09-19 12:52:02 +0700

    Update account.ts

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -56,6 +56,7 @@ export function createAccountRouter(deps: Dependencies): Router {
     logEvent("account_accessed", {
       userId: current.user.id,
       email: current.user.email,
+      expiresAt: current.session.expires_at
     });
   });
 
